6f0d519ced01335d28de451df5ca6bd473c507e4
ledet awano <ledet2016@gmail.com>
2017-07-06 14:06:50 -0500

    Update factorialize.js

--- factorialize.js
+++ factorialize.js
@@ -4,7 +4,7 @@ function factorialize(num) {
   var start = 1    // create a varable that will multiply each number by itself
   for(var i = 1; i <= num ; i++) { // for loop to iterate through the number passed in our argument starting from 
                                    // one and ending at our given num value we pass 
-    start *= i;                    // will multipy each value leading to the numebr by one and by each number
+    start *= i;                // will multipy each value leading to the number by one and by each number
   }
   return start;     
 }
